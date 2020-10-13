c72e16bdbd04238fa7fe18eee7d4c439fae59ef3
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2020-10-13 18:20:05 +0100

    Perform initial static analysis (and fix found issues)

--- src/rebar3_checkshell_prv.erl
+++ src/rebar3_checkshell_prv.erl
@@ -1,6 +1,12 @@
 -module(rebar3_checkshell_prv).
 
--export([init/1, do/1, format_error/1]).
+-export([init/1]).
+-export([do/1]).
+-export([format_error/1]).
+
+-ignore_xref([init/1]).
+-ignore_xref([do/1]).
+-ignore_xref([format_error/1]).
 
 -define(PROVIDER, rebar3_checkshell).
 -define(DEPS, [app_discovery]).
