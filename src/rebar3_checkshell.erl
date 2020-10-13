b67f5478bc9e383c4e1640302416b9a849f5b76f
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2020-10-13 18:12:26 +0100

    Prepare to start development

---
+++ src/rebar3_checkshell.erl
@@ -0,0 +1,10 @@
+-module(rebar3_checkshell).
+
+-export([init/1]).
+
+-spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
+init(State) ->
+    {ok, State1} = rebar3_checkshell_prv:init(State),
+    {ok, State1}.
+
+%FIXME
