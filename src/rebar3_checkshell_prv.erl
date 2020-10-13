5f42d75f896f2ac6a3c32367ac67680558d9c75a
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2020-10-13 21:55:02 +0100

    Handle some FIXME and TODO elements
    Locally, there's dialyzer and compiler options that are turned off

--- src/rebar3_checkshell_prv.erl
+++ src/rebar3_checkshell_prv.erl
@@ -11,30 +11,19 @@
 -define(PROVIDER, rebar3_checkshell).
 -define(DEPS, [app_discovery]).
 
-%% ===================================================================
-%% Public API
-%% ===================================================================
--spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
 init(State) ->
-    Provider = providers:create([
-            {name, ?PROVIDER},            % The 'user friendly' name of the task
-            {module, ?MODULE},            % The module implementation of the task
-            {bare, true},                 % The task can be run by the user, always true
-            {deps, ?DEPS},                % The list of dependencies
-            {example, "rebar3 rebar3_checkshell"}, % How to use the plugin
-            {opts, []},                   % list of options understood by the plugin
-            {short_desc, "A rebar plugin"},
-            {desc, "A rebar plugin"}
-    ]),
+    Provider = providers:create([{name, ?PROVIDER},
+                                 {module, ?MODULE},
+                                 {bare, true},
+                                 {deps, []},
+                                 {desc, "A rebar3 plugin to ease shellcheck'ing"},
+                                 {short_desc, "A rebar3 plugin to ease shellcheck'ing"},
+                                 {example, "rebar3 checkshell"},
+                                 {opts, []}]),
     {ok, rebar_state:add_provider(State, Provider)}.
 
-
--spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
 do(State) ->
     {ok, State}.
 
--spec format_error(any()) ->  iolist().
 format_error(Reason) ->
     io_lib:format("~p", [Reason]).
-
-%FIXME
