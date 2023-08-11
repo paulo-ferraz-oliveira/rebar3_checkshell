854558e1d02729d33f52ed46e1e8c0f793f31a26
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2023-08-11 15:22:46 +0100

    Ease testing

--- src/rebar3_checkshell.erl
+++ src/rebar3_checkshell.erl
@@ -4,8 +4,18 @@
 -export([init/1]).
 -ignore_xref([init/1]).
 
+-export([main/1]).
+-ignore_xref([main/1]).
+
 -spec init(State) -> Result when
     Result :: {ok, State}.
 init(State) ->
     {ok, _Started} = application:ensure_all_started(rebar3_checkshell),
     {ok, _State} = rebar3_checkshell_prv:init(State).
+
+-spec main(Files) -> Result when
+    Files :: [string()],
+    Result :: ok.
+main(Files) ->
+    rebar3_checkshell_prv:do_for(Files, rebar_state:new()),
+    ok.
