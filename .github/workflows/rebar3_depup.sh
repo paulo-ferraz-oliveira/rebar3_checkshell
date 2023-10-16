2637b0a29a8ffc65ada20954ac3eaadf9b6db445
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2023-10-16 23:26:54 +0100

    Respect ShellCheck and sprinkle some Markdown on it

--- .github/workflows/rebar3_depup.sh
+++ .github/workflows/rebar3_depup.sh
@@ -7,24 +7,26 @@ git config user.email "actions@user.noreply.github.com"
 
 BRANCH=feature/rebar3-depup-updates
 
-if git branch -a | grep "$BRANCH" > /dev/null; then
+if git branch -a | grep "${BRANCH}" >/dev/null || true; then
     # already exists
     exit
 fi
 
 git fetch origin
-git checkout -b "$BRANCH"
-mkdir -p "$HOME/.config/rebar3"
-echo "{plugins, [rebar3_depup]}." > "$HOME/.config/rebar3/rebar.config"
+git checkout -b "${BRANCH}"
+mkdir -p "${HOME}/.config/rebar3"
+echo "{plugins, [rebar3_depup]}." >"${HOME}/.config/rebar3/rebar.config"
 rebar3 up
 
-if ! git diff --exit-code 1> /dev/null ; then
+if ! git diff --exit-code 1>/dev/null; then
+    TITLE="[automation] Update \`rebar.config\` versions (via \`rebar3 depup\`)"
+
     # there's stuff to push
     git add rebar.config
-    git commit -m "Update rebar.config versions (rebar3 depup)"
-    git push origin "$BRANCH"
+    git commit -m "${TITLE}"
+    git push origin "${BRANCH}"
 
     gh pr create --fill \
-        --title "Update rebar.config versions with rebar3_depup (automation)" \
+        --title "${TITLE}" \
         --body "This is an automated action to update the repositories rebar.config versions"
 fi
